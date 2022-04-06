81933d653283824a5836d19d4ee37a37955fdd3f
Joseph <joe.kallman@gmail.com>
2022-04-06 17:27:49 +1000

    Character and Scene Update
    Added the 3rd person starter package
    Added the 3rd person character from that package to the scene
    Testing Tile size and camera position, feel free to change them though

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float cameraWalkSpeed, cameraRunSpeed;
+    Vector3 directionRight = new Vector3(1.0f, 0.0f, 0.0f);
+    
+      // Create an enum type so we can pick the speed from a different game manager script
+    public enum Speed
+    {
+        Walk, // 0
+        Run, // 1 
+    };
+
+    public Speed speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraWalkSpeed = 1.0f;
+        cameraRunSpeed = 3.0f;
+    }
+    void Update()
+    {
+        // Check if walking or running to set the correct speed and movement.
+        if (speed == Speed.Walk)
+        {
+            // Create The Movement by direction 
+            Vector3 velocityPerSecond = directionRight.normalized * cameraWalkSpeed;
+            Vector3 velocityThisFrame = velocityPerSecond * Time.deltaTime;
+            transform.position += velocityThisFrame;
+        }
+        else if (speed == Speed.Run) 
+        {
+            Vector3 velocityPerSecond = directionRight.normalized * cameraRunSpeed;
+            Vector3 velocityThisFrame = velocityPerSecond * Time.deltaTime;
+            transform.position += velocityThisFrame;
+        }
+    }
+}
