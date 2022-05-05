5b178e55991fa1ca834d72ef18a5632029fe43c2
goodweathergames <spring.leah11@gmail.com>
2022-05-05 16:21:12 +1000

    Add life total to HUD.  Add invulnerable time to allow player to get away from obstacles.

--- Assets/Scripts/GameManagement/UIManager.cs
+++ Assets/Scripts/GameManagement/UIManager.cs
@@ -7,16 +7,25 @@ public class UIManager : MonoBehaviour {
 
     [SerializeField] private TextMeshProUGUI scoreValueText;
     [SerializeField] private TextMeshProUGUI highScoreValueText;
+    [SerializeField] private TextMeshProUGUI playerLifeValueText;
     
     private ScoreManager scoreManager;
+    private PlayerHealth playerHealth;
     
     private void Start() {
         scoreManager = FindObjectOfType<ScoreManager>();
         scoreManager.OnScoreUpdated += UpdateScoreUI;
+        playerHealth = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
+        playerHealth.OnUpdateHealth += UpdateHealthValue;
         
         UpdateScoreUI(scoreManager.CurrentScore);
 
         highScoreValueText.text = scoreManager.HighScore.ToString();
+        UpdateHealthValue();
+    }
+
+    private void UpdateHealthValue() {
+        playerLifeValueText.text = playerHealth.health.ToString();
     }
 
     private void UpdateScoreUI(int currentScore) {
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,28 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
-{
+public class PlayerHealth : MonoBehaviour {
+    
+    public event Action OnUpdateHealth;
+    
     [Header("Component References")]
     private SpriteRenderer playerSprite; // Temporary to show player death
     public GameManager gameManager;
 
-    [Header("Health Settings")]
-    public float health;
+    [Header("Health Settings")] 
+    [SerializeField] private int maxHealth = 3;
+    [SerializeField] private float invicibleTime = 1.5f;
+    public int health;
     private bool playerIsDead;
     private string gameOverReason;
+    private bool hitObstacle;
 
     void Start()
     {
         playerSprite = GetComponent<SpriteRenderer>();
         playerIsDead = false;
-        health = 3.0f;
+        health = maxHealth;
     }
 
     void Update()
     {
-        if (health <= 0.0f && !playerIsDead)
+        if (health <= 0 && !playerIsDead)
         {
             gameOverReason = "You got hit too many times";
             //TODO: Add different reasons for why Game Over
@@ -42,12 +48,18 @@ public class PlayerHealth : MonoBehaviour
             gameManager.EndGame();
             
         }
-        if (other.gameObject.tag == "Obstacle")
-        {
+        if (other.gameObject.tag == "Obstacle") {
+            gameOverReason = "Out of life";
             gameManager.takeDamageSound.Play();
             health--;
+            OnUpdateHealth?.Invoke();
             Debug.LogError("Hit obstacle"); //TODO: Remove after testing
         }
+    }
+
+    private IEnumerator TurnOffInvincible() {
+        yield return new WaitForSeconds(invicibleTime);
+        hitObstacle = false;
     }
 
     public void Die(string gameOverReasonText)
